8ce59a8050f48e142075578f2ea0fc2c027b237e
marywebby <marywebby1999@gmail.com>
2025-01-28 15:07:53 -0600

    azimbek final outcome

--- simulation.rb
+++ simulation.rb
@@ -5,5 +5,7 @@ shop = CoffeeShop.new
 puts "☕ Welcome to Inventory Java Haven!"
 
 shop.place_order("latte", "medium")
+shop.place_order("cappucino", "large")
+shop.place_order("espresso", "small")
 
 shop.fulfill_orders
